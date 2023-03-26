cc4e90aad2755ab60e8844686dabaa5947a8f6fa
Chris <cgb2@illinois.edu>
2023-03-26 15:33:46 -0500

    Fix block dev

--- setup/infiniswap_bd_setup.sh
+++ setup/infiniswap_bd_setup.sh
@@ -4,7 +4,7 @@ mount -t configfs none /sys/kernel/config
 nbdxadm -o create_host -i 0 -p $PWD/portal.list #portal.list
 nbdxadm -o create_device -i 0 -d 0
 
-ls /dev/infiniswap0
-mkswap /dev/infiniswap0
-swapon /dev/infiniswap0
+ls /dev/sda4
+mkswap /dev/sda4
+swapon /dev/sda4
 
